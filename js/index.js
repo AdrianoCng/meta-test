f17c0746244cd23cd3b0cf0c636a6d65d783ea3d
Adriano Cangiamila <adrianocangiamila@gmail.com>
2021-05-23 14:34:06 +0100

    added fetching API from PHP script and storing results in a database

--- js/index.js
+++ js/index.js
@@ -1,16 +1,47 @@
 const matrix = $("#matrix");
 const matrixReloaded = $("#matrix-reloaded");
 const matrixRevolution = $("#matrix-revolution");
-const carouselInner = $(".carousel-inner");
+const cardDeck = $("#deck");
 
 matrix.on("click", async () => {
-    const { Search } = await (await fetch("http://www.omdbapi.com/?s=Matrix&apikey=720c3666")).json();
+    const urlEncoded = encodeURIComponent("http://www.omdbapi.com/?s=Matrix&apikey=720c3666");
 
-    console.log(Search);
+    const { Search } = await (await fetch(`./php/fetchAPI.php?url=${urlEncoded}`)).json();
 
-    const data = {
-        data: JSON.stringify(Search)
-    }
+    renderCards(Search);
+});
 
-    $.post("./php/movies.php", data);
+matrixReloaded.on("click", async () => {
+    const urlEncoded = encodeURIComponent("http://www.omdbapi.com/?s=Matrix%20Reloaded&apikey=720c3666");
+
+    const { Search } = await (await fetch(`./php/fetchAPI.php?url=${urlEncoded}`)).json();
+
+    renderCards(Search);
+});
+
+matrixRevolution.on("click", async () => {
+    const urlEncoded = encodeURIComponent("http://www.omdbapi.com/?s=Matrix%20Revolutions&apikey=720c3666");
+
+    const { Search } = await (await fetch(`./php/fetchAPI.php?url=${urlEncoded}`)).json();
+
+    renderCards(Search);
 })
+
+
+function renderCards(entries) {
+    cardDeck.html("");
+
+    entries.forEach(({ Poster, Title, Year }) => {
+        cardDeck.append(`
+            <div class="col-3">
+                <div class="card text-center my-4 bg-light">
+                    <img class="card-img-top" src="${Poster === "N/A" ? "https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg" : Poster}" alt="${Title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${Title}</h5>
+                        <p class="card-text">${Year}</p>
+                    </div>
+                </div>
+            </div>
+        `);
+    })
+}
